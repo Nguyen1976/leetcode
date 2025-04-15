fe22ae4011063791a9f6f4065449a81e484936cd
nguyen1976 <nguyen2202794@gmail.com>
2025-04-15 10:53:23 +0700

    implement problem 872-leafSimilar

---
+++ problems/872-leaf-similar-trees.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function (root1, root2) {
+  let getLeaf = (root, leaves = []) => {
+    if (!root) return
+    if (!root.left && !root.right) leaves.push(root.val)
+    getLeaf(root.left, leaves)
+    getLeaf(root.right, leaves)
+    return leaves
+  }
+
+  // let leaves1 = getLeaf(root1)
+  // let leaves2 = getLeaf(root2)
+
+  // if(leaves1.length !== leaves2.length) return false
+
+  // for(let i = 0; i < leaves1.length; i++) {
+  //     if(leaves1[i] !== leaves2[i]) return false
+  // }
+  //Nếu như dùng join trong trường hợp này để so sánh mảng thì sẽ sai với trường hợp mảng là rỗng vì nó luôn luôn return về false
+  return JSON.stringify(getLeaf(root1)) === JSON.stringify(getLeaf(root2))
+}
+
+//solution
+// var leafSimilar = function (root1, root2) {
+//   const dfs = (root) => {
+//     if (root === null) {
+//       return []
+//     }
+
+//     const leaves = dfs(root.left).concat(dfs(root.right))
+
+//     return leaves.length > 0 ? leaves : [root.val]
+//   }
+
+//   return JSON.stringify(dfs(root1)) === JSON.stringify(dfs(root2))
+// }
