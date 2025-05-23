a47500f1bb9cd515acafdc9e5997ac2274037cd5
Nguyen1976 <nguyen2202794@gmail.com>
2025-05-23 09:07:45 +0700

    update problem 62

--- problems/62-unique-paths.js
+++ problems/62-unique-paths.js
@@ -17,8 +17,8 @@ var uniquePaths = function (m, n) {
     dp[0][i] = 1
   }
 
-  for(let i = 0; i < m; i++) {
-    for(let j = 0; j < n; j++) {
+  for(let i = 1; i < m; i++) {
+    for(let j = 1; j < n; j++) {
         dp[i][j] = dp[i-1][j] + dp[i][j-1];
     }
   }
