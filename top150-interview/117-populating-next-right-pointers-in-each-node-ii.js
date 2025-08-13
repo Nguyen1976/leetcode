21a5c7e1b55e753d068db76a1076dffa263a900f
Nguyen1976 <nguyen2202794@gmail.com>
2025-08-13 21:27:24 +0700

    implement problem 117-connect

---
+++ top150-interview/117-populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+  if (!root) return root
+
+  const q = [root]
+  console.log(q)
+
+  while (q.length) {
+    const levelSize = q.length
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = q.shift()
+      if (i < levelSize - 1) node.next = q[0]
+
+      //lấy ra các node của tầng tiếp theo
+      if (node.left) q.push(node.left)
+      if (node.right) q.push(node.right)
+    }
+  }
+  return root
+}
