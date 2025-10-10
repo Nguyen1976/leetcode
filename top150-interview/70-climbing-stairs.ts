3b71d27ca305d934d31fffdce865a17ce6aa6e5e
Nguyen1976 <nguyen2202794@gmail.com>
2025-10-10 15:01:04 +0700

    implement problem 70-climbStairs

---
+++ top150-interview/70-climbing-stairs.ts
@@ -0,0 +1,16 @@
+function climbStairs(n: number): number {
+  if (n <= 1) {
+    return 1
+  }
+
+  let dp = new Array(n + 1).fill(-1)
+  return climbStairsHelper(n, dp)
+}
+
+function climbStairsHelper(n: number, dp: number[]): number {
+  if (n <= 1) return 1
+  if (dp[n] !== -1) return dp[n]
+
+  dp[n] = climbStairsHelper(n - 1, dp) + climbStairsHelper(n - 2, dp)
+  return dp[n]
+}
