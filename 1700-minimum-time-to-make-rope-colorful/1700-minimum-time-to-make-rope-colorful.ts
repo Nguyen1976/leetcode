c9b859a1a4b02fda93eb764f08086ece152eee7e
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2025-11-03 12:03:45 +0700

    Time: 8 ms (45.45%), Space: 67.8 MB (27.27%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.ts
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.ts
@@ -6,26 +6,22 @@ function minCost(colors: string, neededTime: number[]): number {
 
     while(i < arr.length - 1) {
 
-        if(arr[i] === arr[i + 1]) {
-            let k = i + 1
-            while(arr[k] === arr[i]) {
-                k++
-            }
-            //k hiện tại sẽ khác với thằng i tức khoảng từ i -> k là giống nhau
-            let max = neededTime[i]
-            for(let j = i + 1; j < k ; j++) {
-                if(max < neededTime[j]) {
-                    res += max
-                    max = neededTime[j]
-                } else {
-                    res += neededTime[j]
-                }
-            }
-
-            i = k
-        } else {
-            i++
+        let k = i + 1
+        while(arr[k] === arr[i]) {
+            k++
         }
+        //k hiện tại sẽ khác với thằng i tức khoảng từ i -> k là giống nhau
+        let max = neededTime[i]
+        for(let j = i + 1; j < k ; j++) {
+            if(max < neededTime[j]) {
+                res += max
+                max = neededTime[j]
+            } else {
+                res += neededTime[j]
+            }
+        }
+
+        i = k
     }
 
     return res
