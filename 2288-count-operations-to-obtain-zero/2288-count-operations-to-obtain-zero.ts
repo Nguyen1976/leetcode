5e88c0aab0c42b8c076f243b9b97d91e3a31f6df
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2025-11-09 12:40:28 +0700

    Time: 0 ms (100%), Space: 54.1 MB (94.12%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.ts
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.ts
@@ -1,17 +1,12 @@
 function countOperations(num1: number, num2: number): number {
-    if(num1 === 0 || num2 === 0) return 0
-    let res = 0
-    while(num1 !== num2) {
-        res++
-        if(num1 >= num2) {
-            num1 -= num2
-        } else {
-            num2 -= num1
-        }
+    let res = 0;
+    while (num1 && num2) {
+        res += Math.floor(num1 / num2);
+        num1 %= num2;
+        [num1, num2] = [num2, num1];
     }
-    return res + 1
-
-};
+    return res;
+}
 
 //nếu num1 >= num2, phải trừ đi num2 từ num1, nếu không thì trừ num1 từ num2
 //trừ đến khi nào num1 === num2
