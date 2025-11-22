d2c51cc69b24f0b3abec13e4e1b5225617931908
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2025-11-22 10:35:40 +0700

    Time: 2 ms (39.81%), Space: 61.8 MB (63.89%) - LeetHub

---
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.ts
@@ -0,0 +1,14 @@
+function kLengthApart(nums: number[], k: number): boolean {
+    let last = -1; 
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            if (last !== -1 && i - last - 1 < k) {
+                return false;
+            }
+            last = i;
+        }
+    }
+
+    return true;
+}
