5b182d1708996f5c583abe433e2b510c6fc23a44
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2025-11-29 08:04:49 +0700

    Time: 0 ms (100%), Space: 59.1 MB (20%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.ts
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.ts
@@ -1,11 +1,17 @@
 function minOperations(nums: number[], k: number): number {
-    let operations = 0
     let currSum = nums.reduce((acc, curr) => acc + curr, 0)
 
-    while(currSum % k !== 0) {
-        currSum--
-        operations++
-    }
+    // while(currSum % k !== 0) {
+    //     currSum--
+    //     operations++
+    // }
 
-    return operations
+    return currSum % k
 };
+
+/**
+Có thể dùng vòng lặp và tăng cho đến khi %k
+
+hoặc ta có thể làm như sau đầu tiên lấy sum % k và trả về số đó là xong
+
+ */
