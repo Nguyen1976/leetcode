de11b1051b3548c965933498cdefad9b52560eaf
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2025-12-07 21:34:41 +0700

    Time: 0 ms (100%), Space: 56.4 MB (90.91%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.ts
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.ts
@@ -1,9 +1,3 @@
 function countPrefixes(words: string[], s: string): number {
-    let res = 0
-    for(let word of words) {
-        if(word === s.slice(0, word.length)) {
-            res++
-        }
-    }
-    return res
+    return words.filter((word) => s.startsWith(word)).length
 };
