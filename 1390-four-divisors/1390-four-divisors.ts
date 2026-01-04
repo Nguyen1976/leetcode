918c787b983a132bf27a06bdcc641b23f4bfd126
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-01-04 20:20:17 +0700

    Time: 19 ms (71.43%), Space: 59.8 MB (35.71%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.ts
+++ 1390-four-divisors/1390-four-divisors.ts
@@ -1,9 +1,13 @@
 function sumFourDivisors(nums: number[]): number {
     let res = 0
-
+    let cached = new Map<number, number>()
     for (let num of nums) {
         let count = 0
         let sum = 0
+        if(cached.has(num)) {
+            res += cached.get(num)
+            continue
+        }
 
         for (let i = 1; i * i <= num; i++) {
             if (num % i === 0) {
@@ -17,11 +21,15 @@ function sumFourDivisors(nums: number[]): number {
                     sum += i + j
                 }
 
-                if (count > 4) break
+                if (count > 4) {
+                    cached.set(num, 0)
+                    break
+                }
             }
         }
 
         if (count === 4) {
+            cached.set(num, sum)
             res += sum
         }
     }
