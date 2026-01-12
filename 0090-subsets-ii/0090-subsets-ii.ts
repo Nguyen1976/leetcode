92b12a29af926a24704bd1f352b57e48534e8c10
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-01-12 08:20:46 +0700

    Time: 1 ms (86.36%), Space: 60.7 MB (40.5%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.ts
@@ -0,0 +1,19 @@
+function subsetsWithDup(nums: number[]): number[][] {
+    const res = []
+    nums.sort((a, b) => a - b)
+
+    const backtrack = (start, path) => {
+        res.push([...path])
+
+        for(let i = start; i < nums.length; i++) {
+            if (i > start && nums[i] === nums[i - 1]) continue
+            path.push(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+        }
+    }
+
+    backtrack(0, [])
+
+    return res
+};
