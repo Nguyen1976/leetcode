1ffd8a459abe5177a306ad27fb4ce337660e1f74
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-01-26 07:10:36 +0700

    Time: 50 ms (91.15%), Space: 71.9 MB (19.47%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.ts
@@ -0,0 +1,15 @@
+function minimumAbsDifference(arr: number[]): number[][] {
+    arr.sort((a, b) => a - b)
+    let res = []
+    let minDiff = Infinity
+    for(let i = 1; i < arr.length; i++) {
+        minDiff = Math.min(minDiff, arr[i] - arr[i - 1])
+    }
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] - arr[i - 1] === minDiff) {
+            res.push([arr[i - 1], arr[i]])
+        }
+    }
+    return res
+
+};
