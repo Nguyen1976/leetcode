f6abbbd9aab8062813c653b4aa4d66fbe7d5f7dd
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-03-06 09:19:57 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.ts
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.ts
@@ -1,23 +1,9 @@
 function checkOnesSegment(s: string): boolean {
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === '1') {
-            while(s[i] === '1') {
-                i++
-            }
-            while(i < s.length) {
-                if(s[i] === '1') {
-                    return false
-                }
-                i++
-            }
-        }
-    }
-
-
-    return true
+    return !s.includes("01")
 };
 /**
 Tìm ra segment đầu tiên (vị trí kết thúc)
 nếu sau đó gặp segment thì sẽ return false còn không thì sẽ return true
 
+Suy nghĩ kĩ hơn trong chuỗi đã đảm bảo k có số 0 đứng đầu vậy chỉ cần trong chuỗi có 01 thì là false
  */
