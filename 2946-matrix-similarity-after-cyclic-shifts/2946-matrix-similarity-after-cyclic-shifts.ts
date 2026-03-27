8310f42d05dab8d5ed32e8d797ed263f58d041e8
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-03-27 10:42:10 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.ts
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.ts
@@ -1,29 +1,13 @@
 function areSimilar(mat: number[][], k: number): boolean {
     const n = mat.length, m = mat[0].length
-    //cần 1 hàm check row đó sau k lần left ship thì có về chỗ cũ k
-    const check = (i: number) => {
-        const temp: number[] = Array(m).fill(0)
-        for(let j = 0; j < m; j++) {
-            temp[j] = mat[i][(j + k) % m]
-        }
-
-        //check sim
-        for(let j = 0; j < m; j++) {
-            if(temp[j] !== mat[i][j]) return false
-        }
-
-        return true
-    }
-
-    let result = true
+  
     for(let i = 0; i < n; i++) {
-        if(!check(i)) {
-            result = false
-            break
+        for(let j = 0; j < m; j++) {
+            if(mat[i][(j + k) % m] !== mat[i][j]) return false
         }
     }
 
-    return result
+    return true
 };
 
 //cần xác định row 
