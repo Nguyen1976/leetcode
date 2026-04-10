ced7d877f777cb6105a375156d0fba689ff3aeac
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-04-10 13:45:01 +0700

    Time: 7 ms (26.67%), Space: 60 MB (26.67%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.ts
@@ -0,0 +1,20 @@
+function minimumDistance(nums: number[]): number {
+    const map = new Map<number, number[]>()
+    for(let i = 0; i < nums.length; i++) {
+        map.set(nums[i], map.has(nums[i]) ? [...map.get(nums[i]), i] : [i] )
+    }
+
+    let result = Infinity
+
+    for(let [key, value] of map) {
+        if(value.length >= 3) {
+            //cửa sổ trượt
+            for(let i = 0; i <= value.length - 3; i++) {
+                result = Math.min(result, Math.abs(value[i] - value[i + 1]) + Math.abs(value[i + 1] - value[i + 2]) + Math.abs(value[i + 2] - value[i]))
+
+            }
+        }
+    }
+
+    return result === Infinity ? -1 : result
+};
