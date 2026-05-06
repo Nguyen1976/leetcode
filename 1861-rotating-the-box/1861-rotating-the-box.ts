ca07595fa681714948952eb7f85af4c008fe03a4
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-05-06 10:07:51 +0700

    Time: 25 ms (44.44%), Space: 84.1 MB (50%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.ts
+++ 1861-rotating-the-box/1861-rotating-the-box.ts
@@ -1,7 +1,9 @@
 function rotateTheBox(boxGrid: string[][]): string[][] {
     const n = boxGrid.length, m = boxGrid[0].length
-
-    for(let i = 0; i < n; i++) {
+    const result: string[][] = Array.from({ length: m }, () =>
+        Array(n).fill('.')
+    );
+    for(let i = 0; i < n; i++) {    
         let last = m - 1
         for(let j = m - 1; j >= 0; j--) {
             switch(boxGrid[i][j]) {
@@ -10,12 +12,14 @@ function rotateTheBox(boxGrid: string[][]): string[][] {
                     const tmp = boxGrid[i][j];
                     boxGrid[i][j] = boxGrid[i][last];
                     boxGrid[i][last] = tmp;
+                    result[last][n - 1 - i] = '#';
                     last = last - 1
                     //update last
                     break
                 case '.':
                     break
                 case '*':
+                    result[j][n - 1 - i] = '*';
                     last = j - 1
                     break
                 default:
@@ -23,22 +27,6 @@ function rotateTheBox(boxGrid: string[][]): string[][] {
             }
         }
     }
-    const result: string[][] = Array.from({ length: m }, () =>
-        Array(n).fill('')
-    );
-
-    for(let i = 0; i < m; i++) {
-        for(let j = n - 1; j >= 0; j--) {
-            result[i][n - j - 1] = boxGrid[j][i]
-        }
-    }
-    console.log(boxGrid)
 
     return result
 };
-
-/*
-swap cho từng mảng bên trong của ma trận sẽ swap từ dưới
-
-
-*/
