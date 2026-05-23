02cb74ff2e450e7320f0579213eaf8204b6c93b4
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-05-23 15:50:16 +0700

    Time: 0 ms (100%), Space: 55 MB (76.67%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.ts
@@ -0,0 +1,12 @@
+function check(nums: number[]): boolean {
+    let count = 0
+    const n = nums.length
+
+    for(let i = 0; i < n; i++) {
+        if(nums[i] > nums[(i + 1) % n]) {
+            count++
+        }
+    }
+
+    return count <= 1
+}
