babea18ffc4dd28a73eeb61507ad631b28d12c9f
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-05-31 09:04:11 +0700

    Time: 84 ms (75%), Space: 69.2 MB (100%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.ts
+++ 2126-destroying-asteroids/2126-destroying-asteroids.ts
@@ -1,6 +1,7 @@
 function asteroidsDestroyed(mass: number, asteroids: number[]): boolean {
     asteroids.sort((a, b) => a - b)
     for(let ast of asteroids) {
+        if(mass >= asteroids[asteroids.length - 1]) return true
         if(mass >= ast) {
             mass += ast
         } else {
