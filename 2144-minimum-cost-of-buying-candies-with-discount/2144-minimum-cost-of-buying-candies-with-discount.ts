39d1f222040b786bd2459aa854322294b47dccd0
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-01 07:08:33 +0700

    Time: 1 ms (93.33%), Space: 56 MB (60%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.ts
@@ -0,0 +1,13 @@
+function minimumCost(cost: number[]): number {
+    cost.sort((a, b) => b - a);
+
+    let result = 0;
+
+    for (let i = 0; i < cost.length; i++) {
+        if (i % 3 !== 2) {
+            result += cost[i];
+        }
+    }
+
+    return result;
+}
