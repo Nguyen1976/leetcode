748cdbd04f576d8334a448fea67f0ad3254aa406
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-13 15:51:05 +0700

    Time: 3 ms (46.67%), Space: 60.1 MB (12.22%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.ts
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.ts
@@ -1,13 +1,3 @@
 function mapWordWeights(words: string[], weights: number[]): string {
-    let result = ''
-    for(let word of words) {
-        let total = 0
-        for(let c of word) {
-            total += weights[c.charCodeAt(0) - 97]
-        }
-        total %= 26
-        result += String.fromCharCode(97 + 26 - total - 1)
-
-    }
-    return result
+    return words.map(word => String.fromCharCode(97 + 25 - ([...word].reduce((acc, c) => acc + weights[c.charCodeAt(0) - 97], 0) % 26))).join('')
 };
