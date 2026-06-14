48e1ce40782405b7f96703bab903db0cd71371ac
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-14 16:55:42 +0700

    Time: 19 ms (24.39%), Space: 87.9 MB (33.54%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function pairSum(head: ListNode | null): number {
+    let curr = head
+    const arr = []
+    while(curr) {
+        arr.push(curr.val)
+        curr = curr.next
+    }
+    let result = 0
+    for(let i = 0; i < arr.length; i++) {
+        result = Math.max(arr[i] + arr[arr.length - i - 1], result) 
+    }
+
+    return result
+};
