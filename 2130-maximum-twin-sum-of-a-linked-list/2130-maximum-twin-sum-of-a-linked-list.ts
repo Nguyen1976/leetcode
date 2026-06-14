8ee992d509a107c5902621718ed8d2a4ad73ea6b
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-14 17:07:01 +0700

    Time: 107 ms (5.49%), Space: 90.4 MB (14.63%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.ts
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.ts
@@ -9,18 +9,69 @@
  *     }
  * }
  */
+class ListNode2 {
+    val: number;
+    next: ListNode2 | null;
+    prev: ListNode2 | null;
+
+    constructor(
+        val?: number,
+        next?: ListNode2 | null,
+        prev?: ListNode2 | null
+    ) {
+        this.val = val ?? 0;
+        this.next = next ?? null;
+        this.prev = prev ?? null;
+    }
+}
+
+function buildDoublyLinkedList(head: ListNode | null): ListNode2 | null {
+    if (!head) return null;
+
+    const dummyHead = new ListNode2(head.val);
+    let curr1 = head.next;
+
+    let curr2 = dummyHead;
+
+    while (curr1) {
+        const node = new ListNode2(curr1.val);
+
+        curr2.next = node;
+        node.prev = curr2;
+
+        curr2 = node;
+        curr1 = curr1.next;
+    }
+
+    return dummyHead;
+}
 
 function pairSum(head: ListNode | null): number {
-    let curr = head
-    const arr = []
-    while(curr) {
-        arr.push(curr.val)
-        curr = curr.next
-    }
-    let result = 0
-    for(let i = 0; i < Math.floor(arr.length / 2); i++) {
-        result = Math.max(arr[i] + arr[arr.length - i - 1], result) 
+    let n = 0;
+    let curr = head;
+
+    while (curr) {
+        n++;
+        curr = curr.next;
     }
 
-    return result
-};
+    let left = buildDoublyLinkedList(head);
+    if (!left) return 0;
+
+    let right = left;
+
+    while (right.next) {
+        right = right.next;
+    }
+
+    let result = 0;
+
+    for (let i = 0; i < n / 2; i++) {
+        result = Math.max(result, left.val + right.val);
+
+        left = left.next!;
+        right = right.prev!;
+    }
+
+    return result;
+}
