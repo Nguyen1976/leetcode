90e409cc0642bb7ae23ddf657b86f884a9ad9e7d
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-18 10:08:47 +0700

    Time: 0 ms (100%), Space: 55.7 MB (0%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.ts
@@ -0,0 +1,18 @@
+function angleClock(hour: number, minutes: number): number {
+    if(hour === 12) {
+        hour = 0
+    }
+
+    return (Math.abs(minutes - (hour * 5 + 5 / (60 / minutes))) * 360 / 60) > 180 ? 360 - (Math.abs(minutes - (hour * 5 + 5 / (60 / minutes))) * 360 / 60) : (Math.abs(minutes - (hour * 5 + 5 / (60 / minutes))) * 360 / 60)
+};
+
+/**
+ta hiểu đồng hồ sẽ có 360 độ tương đương với 60 phút
+cần xác định vị trí kim phút các vị trí kim giờ
+ví dụ 12h30 tức kím giờ chỉ ở chỗ 5' đầu rồi chia đôi sẽ ra vị trí
+
+xác định 0 giờ hay 12 giờ là như nhay
+ví dụ 12h sẽ đổi hết thành 0h để xác định vị trí kimgiowf ở phút bao nhiêu
+giả dụ 12h sẽ là ở phút só 0 1 giờ ở phút số 5
+cộng thêm với 60 chia cho số phút thì sẽ ra được vị trí chính xác số phút của kim giờ
+ */
