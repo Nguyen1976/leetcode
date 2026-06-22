ddf73705b02a0334bfa49f4a5825c69535a25ad2
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-06-22 13:36:08 +0700

    Time: 10 ms (16.48%), Space: 57.6 MB (81.32%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.ts
@@ -3,17 +3,11 @@ function maxNumberOfBalloons(text: string): number {
     for(let c of text) {
         map.set(c, map.has(c) ? map.get(c) + 1 : 1)
     }
-    let result = 0
-    while(map.get('b') >= 1 && map.get('a') >= 1 && map.get('l') >= 2 && map.get('o') >= 2 && map.get('n') >= 1) {
-        //trừ đi 1:b, 1:a, 2:l, 2:o, 1:n
-
-        map.set('b', map.get('b') - 1)
-        map.set('a', map.get('a') - 1)
-        map.set('l', map.get('l') - 2)
-        map.set('o', map.get('o') - 2)
-        map.set('n', map.get('n') - 1)
-        result++ 
-    }
-
-    return result
+    return Math.min(
+        map.get('b') ?? 0,
+        map.get('a') ?? 0,
+        Math.floor((map.get('l') ?? 0) / 2),
+        Math.floor((map.get('o') ?? 0) / 2),
+        map.get('n') ?? 0
+    );
 };
