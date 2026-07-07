9d353045d7231576ca7dd5bb5b98fc939d17ad24
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-07-07 11:52:56 +0700

    Time: 1 ms (60%), Space: 58.2 MB (13.33%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.ts
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.ts
@@ -1,12 +1,19 @@
 function sumAndMultiply(n: number): number {
-    let sum = 0
-    let digit = ''
-    for(let c of String(n)) {
-        if(c !== '0') {
-            digit += c
-            sum += Number(c)
+    let sum = 0;
+    let num = 0;
+    let place = 1;
+
+    while (n > 0) {
+        const digit = n % 10;
+
+        if (digit !== 0) {
+            sum += digit;
+            num += digit * place;
+            place *= 10;
         }
+
+        n = Math.floor(n / 10);
     }
 
-    return Number(digit) * sum
-};
+    return sum * num;
+}
