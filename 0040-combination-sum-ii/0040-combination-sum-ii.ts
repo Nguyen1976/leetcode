54b835e185779e9dff07a6bfe0ff8dd9a3b0665d
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-08-09 19:13:33 +0700

    Time: 8 ms (18.15%), Space: 58.6 MB (84.27%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.ts
+++ 0040-combination-sum-ii/0040-combination-sum-ii.ts
@@ -1,29 +1,31 @@
 function combinationSum2(candidates: number[], target: number): number[][] {
-    let results = []
-    candidates.sort((a, b) => a - b);
+    const n = candidates.length;
+    const results: number[][] = [];
+    candidates.sort((a, b) => a - b)
 
-    let backtrack = (start, remain, path) => {
-        if(remain === 0) {
-            results.push([...path])
-            return
+    const backtrack = (idx: number, path: number[], total: number) => {
+        if (total === target) {
+            results.push([...path]);
+            return;
+        }
+        
+        if (total > target) {
+            return;
         }
 
-        for(let i = start; i < candidates.length; i++) {
-            //trong cùng 1 tầng thì k để trùng nhau tránh tạo ra kết quả duplicate
-            //tức tầng này bắt đầu từ 1 thì vòng lặp sau bắt đầu từ 1 gây ra duplicate
-            if (i > start && candidates[i] === candidates[i - 1]) continue
-
-            if (candidates[i] > remain) break
-
-            path.push(candidates[i])
-
-            backtrack(i + 1, remain - candidates[i], path)
-
-            path.pop()
+        for (let i = idx; i < n; i++) {
+            if (i > idx && candidates[i] === candidates[i - 1]) {
+                continue;
+            }
+            path.push(candidates[i]);
+            
+            backtrack(i + 1, path, total + candidates[i]);
+            
+            path.pop();
         }
     }
 
-    backtrack(0, target, [])
+    backtrack(0, [], 0);
 
-    return results
+    return results;
 };
