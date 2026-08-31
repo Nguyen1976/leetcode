1a3ad4d86bb5b582620e9ad3975fb0a1d590644d
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-08-31 18:13:52 +0700

    Time: 3 ms (100%), Space: 82.2 MB (83.33%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.ts
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.ts
@@ -1,43 +1,42 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
 function nodesBetweenCriticalPoints(head: ListNode | null): number[] {
-    let curr = head.next
-    let prev = head.val
-    
-    let arr = []
-    let idx = 1
-    while(curr.next) {
-        if((curr.val > prev && curr.val > curr.next.val) || (curr.val < prev && curr.val < curr.next.val)) {
-            arr.push(idx)
+    let curr = head;
+    let prev = curr.val;
+    curr = curr.next;
+
+    let idx = 1;
+
+    let firstCritical = -1;
+    let prevCritical = -1;
+    let minDistance = Infinity;
+
+    while (curr.next) {
+        if (
+            (curr.val > prev && curr.val > curr.next.val) ||
+            (curr.val < prev && curr.val < curr.next.val)
+        ) {
+            if (firstCritical === -1) {
+                firstCritical = idx;
+            } else {
+                minDistance = Math.min(
+                    minDistance,
+                    idx - prevCritical
+                );
+            }
+
+            prevCritical = idx;
         }
-        idx++
-        prev = curr.val
-        curr = curr.next
+
+        idx++;
+        prev = curr.val;
+        curr = curr.next;
     }
 
-    if(arr.length < 2) return [-1, -1]
-
-    let minDistance = Infinity
-    for(let i = 1; i < arr.length; i++) {
-        minDistance = Math.min(minDistance, arr[i] - arr[i - 1])
+    if (minDistance === Infinity) {
+        return [-1, -1];
     }
 
-    return [minDistance, arr[arr.length - 1] - arr[0]]
-};
-/**
-local max là lớn hơn hơn node trước nó và sau nó
-local min là nhỏ hơn node trước nó và sau nó
-
-hướng giải sẽ tìm ra vị trí của toàn bộ local min và local max
-sau đó sẽ tìm ra min và max distance
- */
+    return [
+        minDistance,
+        prevCritical - firstCritical
+    ];
+}
