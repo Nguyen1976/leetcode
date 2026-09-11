281af6b187cdf7adb0345ce5606438f5d4d32e76
Nguyễn Hà Nguyên <Nguyen2202794@gmail.com>
2026-09-11 15:22:54 +0700

    Time: 34 ms (99.79%), Space: 55.8 MB (53.32%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.ts
@@ -0,0 +1,14 @@
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    const number1 = await promise1
+    const number2 = await promise2
+    return new Promise(
+       resolve => resolve(number1 + number2)
+    )
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
